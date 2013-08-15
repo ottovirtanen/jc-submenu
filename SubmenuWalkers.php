be268c5f0f6d891a761e36ae1d8bd41e0a7c280d
jcollings <jcollings@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-15 17:58:01 +0000

    Removed unused dropdown walker class
    git-svn-id: http://plugins.svn.wordpress.org/jc-submenu/trunk@756798 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- SubmenuWalkers.php
+++ SubmenuWalkers.php
@@ -581,32 +581,4 @@ class JC_Submenu_Nav_Walker extends Walker_Nav_Menu {
 	}
 }
 
-class Walker_Nav_Menu_Dropdown extends JC_Submenu_Nav_Walker{
- 
-	var $item_id = 0;
- 
-	function __construct($id = 0){
-		$this->item_id = $id;
-	}
- 
-	public function start_lvl(&$output, $depth){}
- 
-	public function end_lvl(&$output, $depth){}
- 
-	public function start_el(&$output, $item, $depth, $args){
- 
-		$item->title = str_repeat("&nbsp;", $depth * 4) . $item->title;
- 
-		parent::start_el($output, $item, $depth, $args);
-		if($item->ID == $this->item_id)
-			$output = str_replace('<li', '<option value="'.$item->ID.'" selected="selected"', $output);
-		else
-			$output = str_replace('<li', '<option value="'.$item->ID.'"', $output);
-	}
- 
-	public function end_el(&$output, $item, $depth){
-		$output .= "</option>\n";
-	}
-}
-
 ?>
